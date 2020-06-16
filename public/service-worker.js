3f9682322334cf82ef2f909c9db520c3a0aeca49
Alan Lee <alanleeone@gmail.com>
2020-06-16 16:03:35 -0700

    added manifest

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open("list-cache-v1")
+      .then((cache) =>
+        cache.addAll(["/", "/db.min.js", "/app.min.js", "/manifest.json"])
+      )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch((err) => {
+      return caches.match(event.request).then((match) => {
+        if (match) {
+          return match;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
